05e07cc8410edb9026fb8df844a7c3b0548f9279
Marcos Barbosa <mvpbarbosa16@gmail.com>
2024-11-13 15:45:02 -0300

    try again and score functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
+const score = document.querySelector('.score');
+const bestScore = document.querySelector('.best-score');
+const gameBoard = document.querySelector('.game-board');
+const tryAgain = document.querySelector('.try-again');
+
+let points = 0;
+let bestPoints = 0;
+
+document.cookie = 'chave=valor; expires=Mon, 31 Dec 2024 23:59:59 GMT;path=/';
 
 const jump = () => {
     mario.classList.add('jump');
@@ -32,10 +41,42 @@ const loop = setInterval(() => {
         mario.style.width = '65px';
         mario.style.marginLeft = '55px';
 
+        if (bestPoints <= points) {
+            bestPoints = points;
+            bestScore.textContent = `BEST SCORE: ${bestPoints - 1}`;
+        } else {
+            bestPoints = sessionStorage.getItem('best-points-score');
+            bestScore.textContent = `BEST SCORE: ${bestPoints - 1}`;
+        }
+
         clearInterval(loop);
+        clearInterval(scorePoints);
+
+        tryAgain.style.display = 'block';
+        setInterval(() => {
+            tryAgain.style.transform = 'scale(1)';
+        }, 50);
+
+        sessionStorage.setItem('best-points-score', bestPoints);
     }
 }, 10);
 
+if (sessionStorage.getItem('best-points-score') === false) {
+    sessionStorage.setItem('best-points-score', bestPoints - 1);
+} else {
+    bestPoints = sessionStorage.getItem('best-points-score');
+}
 
+if (bestPoints != 0) {
+    bestScore.textContent = `BEST SCORE: ${bestPoints -1}`;
+}
+const scorePoints = setInterval(() => {
+    score.textContent = `SCORE: ${points}`;
+    points++;
+}, 100);
+
+tryAgain.addEventListener('click', () => {
+    window.location.reload();
+});
 
 document.addEventListener('keydown', jump);
